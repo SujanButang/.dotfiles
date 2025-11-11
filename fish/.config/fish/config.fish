913f5ac23e4a938d3f89e81cd7ff5e05fec9daa3
SujanButang <sujanbutang@gmail.com>
2025-11-11 17:07:57 +0545

    added yazi as CLI file explorer

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,23 @@
+# Run commands only in interactive sessions
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Make PostgreSQL tools available globally
+set -gx PATH /Library/PostgreSQL/18/bin $PATH
+
+# Make Go binaries (like sqlc) available globally
+set -gx PATH /go/bin $PATH
+
+export EDITOR='nvim'
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
+starship init fish | source
